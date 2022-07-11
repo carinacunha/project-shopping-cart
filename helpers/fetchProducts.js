e93e26be9a5cf545821b6de0398415113356d435
Carina Cunha <carina.cunha13@gmail.com>
2022-07-11 18:06:07 -0300

    Requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -5,6 +5,27 @@ const fetchProducts = async () => {
   return data;
 };
 
+const createProductItemElement = async () => {
+  const items = await fetchProducts();
+  const itemsList = document.getElementsByClassName('items')[0];
+  items.results.forEach((item) => {
+    const block = document.createElement('div');
+    block.className = 'item';
+    const ident = document.createElement('ul');
+    const name = document.createElement('ul');
+    const image = document.createElement('img');
+    const { id, title, thumbnail } = item;
+    ident.innerText = id;
+    name.innerHTML = title;
+    image.src = thumbnail;
+    itemsList.appendChild(block);
+    block.appendChild(ident);
+    block.appendChild(name);
+    block.appendChild(image);
+  });
+};
+
+console.log(createProductItemElement());
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
